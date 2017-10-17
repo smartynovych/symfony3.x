bcd047ec4bd9a55e1b68dae02ccde07ecc5f9233
Serhiy Martynovych <smartynovych@gmail.com>
2017-10-17 13:06:16 +0300

    Create Unit and functional test for Article Entity and Controller

--- tests/AppBundle/Controller/ArticleControllerTest.php
+++ tests/AppBundle/Controller/ArticleControllerTest.php
@@ -2,35 +2,168 @@
 
 namespace Tests\AppBundle\Controller;
 
-use AppBundle\Controller\ArticleController;
-use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use AppBundle\Entity\Article;
+use Faker\Factory;
 
-class ArticleControllerTest extends TestCase
+class ArticleControllerTest extends WebTestCase
 {
-    /**
-     * @var \ArticleController $article
-     */
-    private $article;
 
     /**
-     * {@inheritdoc}
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    /**
+     * {@inheritDoc}
      */
     protected function setUp()
     {
-        parent::setUp();
+        self::bootKernel();
+
+        $this->em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
     }
 
-
-    public function testIndex()
+    public function testCreateArticle()
     {
-        $this->article = new ArticleController();
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/article/create');
 
-        //$this->assertEquals(200, $article->getResponse()->getStatusCode());
-        //$this->assertContains('Welcome to Symfony', $article->filter('#container h1')->text());
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("Create new article")')->count()
+        );
 
-        $result = 42;
+        $token = $crawler->filter('input[name="article[_token]"]')->extract(array('value'));
 
-        // assert that your calculator added the numbers correctly!
-        $this->assertEquals(42, $result);
+        $faker = Factory::create('ru_RU');
+
+        $crawler = $client->request('POST', '/article/create', array(
+            'article' => array(
+                'name' => $faker->name,
+                'description' => $faker->text,
+                'createdAt' => array (
+                    'date' => (array(
+                        'month' => mt_rand(1,12),
+                        'day' => mt_rand(1,28),
+                        'year' => mt_rand(2012,2017),
+                        )
+                    ),
+                    'time' => (array(
+                        'hour' => mt_rand(1,12),
+                        'minute' => mt_rand(1,60),
+                    )
+                )),
+                '_token' => $token[0],
+                'save' => 'Save',
+            )));
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+    }
+
+    public function testIndexArticle()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/article/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testDetailArticle()
+    {
+        $articles = $this->em->getRepository(Article::class)->findAll();
+        $article = array_pop($articles);
+
+        if($article) {
+            $client = static::createClient();
+
+            $client->request('GET', '/article/detail/'.$article->getId());
+
+            $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        } else {
+            $this->assertTrue($article, 'No article found');
+        }
+    }
+
+    public function testUpdateArticle()
+    {
+        $articles = $this->em->getRepository(Article::class)->findAll();
+        $article = array_pop($articles);
+
+        if($article) {
+            $client = static::createClient();
+
+            $crawler = $client->request('GET', '/article/update/'.$article->getId());
+
+            $this->assertGreaterThan(
+            0,
+                $crawler->filter('html:contains("Update article #'.$article->getId().'")')->count()
+            );
+
+            $token = $crawler->filter('input[name="article[_token]"]')->extract(array('value'));
+
+            $faker = Factory::create('ru_RU');
+
+            $crawler = $client->request('POST', '/article/update/'.$article->getId(), array(
+                'article' => array(
+                    'name' => 'UPDATE #'.$article->getId(). ': '.$faker->name,
+                    'description' => 'UPDATE '.$faker->text,
+                    'createdAt' => array (
+                        'date' => (array(
+                            'month' => mt_rand(1,12),
+                            'day' => mt_rand(1,28),
+                            'year' => mt_rand(2012,2017),
+                        )
+                        ),
+                        'time' => (array(
+                            'hour' => mt_rand(1,12),
+                            'minute' => mt_rand(1,60),
+                        )
+                        )),
+                    '_token' => $token[0],
+                    'save' => 'Save',
+                )));
+
+            // TODO: msg 171017 не получаем статус редиректа, в браузер выбрасывается сообщение Redirect to /article/, возможно из-за маршрутизации в контероллере
+            //$this->assertEquals(302, $client->getResponse()->getStatusCode());
+
+            $this->assertGreaterThan(
+                0,
+                $crawler->filter('html:contains("Redirect")')->count()
+            );
+
+        } else {
+            $this->assertTrue($article, 'No article found');
+        }
+    }
+
+    public function testDeleteArticle()
+    {
+        $articles = $this->em->getRepository(Article::class)->findAll();
+        $article = array_pop($articles);
+
+        if($article) {
+            $client = static::createClient();
+
+            $client->request('GET', '/article/delete/'.$article->getId());
+
+            $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        } else {
+            $this->assertTrue($article, 'No article found');
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->em->close();
+        $this->em = null; // avoid memory leaks
     }
 }
--- tests/AppBundle/Entity/ArticleTest.php
+++ tests/AppBundle/Entity/ArticleTest.php
@@ -4,7 +4,7 @@ namespace Tests\AppBundle\Entity;
 
 use AppBundle\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
-use Doctrine\ORM\Mapping as ORM;
+use Faker\Factory;
 
 class ArticleTest extends TestCase
 {
@@ -12,6 +12,7 @@ class ArticleTest extends TestCase
      * @var Article $article
      */
     private $article;
+    private $faker;
 
     /**
      * {@inheritdoc}
@@ -19,18 +20,69 @@ class ArticleTest extends TestCase
     protected function setUp()
     {
         $this->article = new Article();
-        $this->article->setName('TEST: top article');
-        $this->article->setDescription('TEST: this text for article description');
-        $this->article->setCreatedAt(new \DateTime());
+        $this->faker = Factory::create('ru_RU');
     }
 
-
+    /**
+     * Try to set and get name
+     */
     public function testName()
     {
-        $article = new Article();
-        $article->setName('TEST: name');
+        $name = $this->faker->name;
+        $this->article->setName($name);
 
-        $this->assertEquals('TEST: name', $article->getName());
+        $this->assertEquals($name, $this->article->getName());
+    }
+
+    /**
+     * Try to set and get description
+     */
+    public function testDescription()
+    {
+        $description = $this->faker->text;
+        $this->article->setDescription($description);
+
+        $this->assertEquals($description, $this->article->getDescription());
+    }
+
+    /**
+     * Try to set and get createdAt
+     */
+    public function testCreatedAt()
+    {
+        $createdAt = new \DateTime();
+        $this->article->setCreatedAt($createdAt);
+
+        $this->assertEquals($createdAt->format('d.m.Y H:i:s'), $this->article->getCreatedAt()->format('d.m.Y H:i:s'));
+    }
+
+    /**
+     * Try to set and get categoryId
+     */
+    public function testCategoryId()
+    {
+        $this->article->setCategoryId(1);
+
+        $this->assertEquals(1, $this->article->getCategoryId());
+    }
+
+    /**
+     * Try to set and get languageId
+     */
+    public function testLanguageId()
+    {
+        $this->article->setLanguageId('ru');
+
+        $this->assertEquals('ru', $this->article->getLanguageId());
+    }
+
+    /**
+     * Try to set and get isActive
+     */
+    public function testIsActive()
+    {
+        $this->article->setIsActive('Y');
+
+        $this->assertEquals('Y', $this->article->getIsActive());
     }
 }
-;
