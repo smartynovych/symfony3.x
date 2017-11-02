d5ccf270787b687eea4629431b9f51de0046272d
Serhiy Martynovych <smartynovych@gmail.com>
2017-11-02 15:06:56 +0200

    Send notification after article create and update

--- src/AppBundle/EventListener/IpTraceSubscriber.php
+++ src/AppBundle/EventListener/IpTraceSubscriber.php
@@ -8,6 +8,15 @@ use AppBundle\Entity\Article;
 
 class IpTraceSubscriber implements EventSubscriber
 {
+    protected $twig;
+    protected $mailer;
+
+    public function __construct(\Twig_Environment $twig, \Swift_Mailer $mailer)
+    {
+        $this->twig = $twig;
+        $this->mailer = $mailer;
+    }
+
     public function getSubscribedEvents()
     {
         return array(
@@ -31,6 +40,27 @@ class IpTraceSubscriber implements EventSubscriber
         $entity = $args->getObject();
 
         if ($entity instanceof Article) {
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Article ' . $entity->getId() . ' updated')
+                ->setFrom('noreply@example.com')
+                ->setTo('dev@example.com')
+                ->setBody(
+                    $this->twig->render(
+                        'mail/article.html.twig',
+                        array('name' => $entity->getName())
+                    ),
+                    'text/html'
+                )
+                ->addPart(
+                    $this->twig->render(
+                        'mail/article.txt.twig',
+                        array('name' => $entity->getName())
+                    ),
+                    'text/plain'
+                )
+            ;
+            $this->mailer->send($message);
+
             // Тут добавляем код для выполнения реакции на событие
             //$entityManager = $args->getEntityManager();
             //$article = $entityManager->getRepository(Article::class)->find(1);
